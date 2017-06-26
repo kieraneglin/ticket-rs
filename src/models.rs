e93ed19f3210cfed9a5d1d6e035f65be1b4f4de1
Kieran Eglin <kieran.eglin@gmail.com>
2017-06-26 16:29:27 -0700

    basic CLI for adding tickets

--- src/models.rs
+++ src/models.rs
@@ -1,17 +1,10 @@
-use super::schema::{users, tickets};
+use super::schema::tickets;
 
 #[derive(Queryable)]
 pub struct User {
     pub id: i32,
     pub name: String,
     pub email: String,
-}
-
-#[derive(Insertable)]
-#[table_name = "users"]
-pub struct NewUser<'a> {
-    pub name: &'a str,
-    pub email: &'a str,
 }
 
 #[derive(Insertable)]
